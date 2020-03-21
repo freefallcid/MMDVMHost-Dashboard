d2cf6e96bb1d5c551ea9d4a000e237ae850f345e
Dominic Reich <freefallcid@users.noreply.github.com>
2020-03-21 22:16:23 +0100

    Update dbupdate.sh
    Uses pi-star DMRIds.dat for updates on the callsign db

--- database/dbupdate.sh
+++ database/dbupdate.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 echo Downloading DMR-IDs from RadioID database
 #curl 'https://www.ham-digital.org/status/users.csv' 2>/dev/null | sed -e 's/\t//g' | awk -F"," '/,/{gsub(/ /, "", $2); printf "%s;%s;%s\n", $1, $2, $3}' | sed -e 's/\(.\) .*/\1/g' > dmrids.dat
-curl 'https://ham-digital.org/status/dmrid.dat' 2>/dev/null > dmrids.dat
+#curl 'https://ham-digital.org/status/dmrid.dat' 2>/dev/null > dmrids.dat
+curl 'http://www.pistar.uk/downloads/DMRIds.dat' 2>/dev/null > dmrids_pistar.dat
+
+cat dmrids_pistar.dat| awk '{print $1";"$2";"$3$4$5}' > dmrids.dat
 
 echo Removing IDs from local database
 echo -e 'delete from callsign where 1;' | sqlite3 callsigns.db
 
-echo inserting new ID-list into local database
+echo Inserting new ID-list into local database
 echo -e '.separator ";" \n.import dmrids.dat callsign' | sqlite3 callsigns.db
 
