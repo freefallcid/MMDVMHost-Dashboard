0721f703a574309e72942e9299c7801bd44e50a5
Dominic Reich <freefallcid@users.noreply.github.com>
2020-03-22 07:57:48 +0100

    Update dbupdate.sh
    Use pistar DMRIds.dat file

--- database/dbupdate.sh
+++ database/dbupdate.sh
@@ -2,13 +2,14 @@
 echo Downloading DMR-IDs from RadioID database
 #curl 'https://www.ham-digital.org/status/users.csv' 2>/dev/null | sed -e 's/\t//g' | awk -F"," '/,/{gsub(/ /, "", $2); printf "%s;%s;%s\n", $1, $2, $3}' | sed -e 's/\(.\) .*/\1/g' > dmrids.dat
 #curl 'https://ham-digital.org/status/dmrid.dat' 2>/dev/null > dmrids.dat
-curl 'http://www.pistar.uk/downloads/DMRIds.dat' 2>/dev/null > dmrids_pistar.dat
+curl 'http://www.pistar.uk/downloads/DMRIds.dat' 2>/dev/null > /var/www/html/database/dmrids_pistar.dat
 
-cat dmrids_pistar.dat| awk '{print $1";"$2";"$3$4$5}' > dmrids.dat
+cat /var/www/html/database/dmrids_pistar.dat| awk '{print $1";"$2";"$3$4$5}' > /var/www/html/database/dmrids.dat
 
 echo Removing IDs from local database
-echo -e 'delete from callsign where 1;' | sqlite3 callsigns.db
+echo -e 'delete from callsign where 1;' | sqlite3 /var/www/html/database/callsigns.db
 
 echo Inserting new ID-list into local database
-echo -e '.separator ";" \n.import dmrids.dat callsign' | sqlite3 callsigns.db
+echo -e '.separator ";" \n.import dmrids.dat callsign' | sqlite3 /var/www/html/database/callsigns.db
 
+rm -f /var/www/html/database/dmrids_pistar.dat /var/www/html/database/dmrids.dat
