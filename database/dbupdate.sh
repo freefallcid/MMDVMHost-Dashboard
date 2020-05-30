9d86d0abb87aec13f164ed7ceb0a6111db706523
Dominic Reich <freefallcid@users.noreply.github.com>
2020-05-30 19:15:02 +0200

    update dbupdate.sh and callsigns.db

--- database/dbupdate.sh
+++ database/dbupdate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-echo Downloading DMR-IDs from RadioID database
-curl 'https://ham-digital.org/status/users.csv' 2>/dev/null | sed -e 's/\t//g' | awk -F"," '/,/{gsub(/ /, "", $2); printf "%s\t%s\t%s\n", $1, $2, $3}' | sed -e 's/\(.\) .*/\1/g' > /var/www/html/database/dmrids.dat
+#echo Downloading DMR-IDs from RadioID database
+#curl 'https://ham-digital.org/status/users.csv' 2>/dev/null | sed -e 's/\t//g' | awk -F"," '/,/{gsub(/ /, "", $2); printf "%s\t%s\t%s\n", $1, $2, $3}' | sed -e 's/\(.\) .*/\1/g' > /var/www/html/database/dmrids.dat
 #curl 'https://ham-digital.org/status/dmrid.dat' 2>/dev/null > dmrids.dat
+
+echo Downloading DMR-IDs from DMR-Database at Github
+curl 'https://raw.githubusercontent.com/DMR-Database/database-beta/master/userat.csv' 2>/dev/null | sed -e 's/\t//g' | awk -F"," '/,/{gsub(/ /, "", $2); printf "%s\t%s\t%s\n", $1, $2, $3}' | sed -e 's/\(.\) .*/\1/g' > /var/www/html/database/dmrids.dat
 
 #echo Downloading DMR-IDs from Pi-Star database
 #curl 'http://www.pistar.uk/downloads/DMRIds.dat' 2>/dev/null > /var/www/html/database/dmrids_pistar.dat
