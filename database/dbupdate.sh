0b3e9745808515a0219c98e2233c4b684864bcbe
Dominic Reich <freefallcid@users.noreply.github.com>
2020-05-30 18:20:56 +0200

    use tab instead of ";" in dmrids.dat

--- database/dbupdate.sh
+++ database/dbupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Downloading DMR-IDs from RadioID database
-curl 'https://ham-digital.org/status/users.csv' 2>/dev/null | sed -e 's/\t//g' | awk -F"," '/,/{gsub(/ /, "", $2); printf "%s;%s;%s\n", $1, $2, $3}' | sed -e 's/\(.\) .*/\1/g' > /var/www/html/database/dmrids.dat
+curl 'https://ham-digital.org/status/users.csv' 2>/dev/null | sed -e 's/\t//g' | awk -F"," '/,/{gsub(/ /, "", $2); printf "%s\t%s\t%s\n", $1, $2, $3}' | sed -e 's/\(.\) .*/\1/g' > /var/www/html/database/dmrids.dat
 #curl 'https://ham-digital.org/status/dmrid.dat' 2>/dev/null > dmrids.dat
 
 #echo Downloading DMR-IDs from Pi-Star database
@@ -12,7 +12,7 @@ echo Removing IDs from local database
 echo -e 'delete from callsign where 1;' | sqlite3 /var/www/html/database/callsigns.db
 
 echo Inserting new ID-list into local database
-echo -e '.separator ";" \n.import /var/www/html/database/dmrids.dat callsign' | sqlite3 /var/www/html/database/callsigns.db
+echo -e '.separator "\t" \n.import /var/www/html/database/dmrids.dat callsign' | sqlite3 /var/www/html/database/callsigns.db
 
 echo Removing temporary files
 cp /var/www/html/database/dmrids.dat /usr/local/etc/DMRIds.dat
